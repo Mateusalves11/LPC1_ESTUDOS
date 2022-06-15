2287e77d2a0b634aedc68a19ff4e015115471f81
Mateus Alves <100079925+Mateusalves11@users.noreply.github.com>
2022-06-15 12:34:27 -0300

    Add files via upload

---
+++ PYTHON/asdasd.py
@@ -0,0 +1,10 @@
+#7. Faça um Programa que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
+lista=[1,2,3,4,5]
+soma = sum(lista)
+soma1= soma.list
+for x in lista:
+  x=x*x
+  mult = x
+  print(mult)
+
+
---
+++ PYTHON/exercio9.py
@@ -0,0 +1,7 @@
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string,
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e
+# o quarto elemento um valor do tipo float.
+# Imprima a lista na tela.
+
+list1=('arroz',(45,5433),{'mateus':23,'arroz':'batata'},3.14)
+print (list1)
